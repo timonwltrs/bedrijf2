d6bbb9f64cb55fa5e7e015e0171336783d286d8d
timonwltrs <9012910@student.zadkine.nl>
2021-11-02 09:33:04 +0100

    2-11

---
+++ dames.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bedrijfs website</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="shortcut icon" href="img/logo2.png">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  
+</head>
+
+<body>
+  <nav>
+    <ul>
+      <center>
+        <img src="img/zalando1.png" alt="logo" width="150" height="35">
+      </center>
+      <li><a href="index.php">Home</a></li>
+      <li class="dropdown">
+        <a href="javascript:void(0)" class="schoenen1">Schoenen</a>
+        <section class="content">
+          <a href="dames.php">Dames</a>
+          <a href="heren.php">Heren</a>
+          <a href="kinderen.php">Kinderen</a>
+      </li>
+      <li><a href="contact.php">Contact</a></li>
+
+      <section class="searchbar">
+  <input type="text" placeholder="search..">
+  <button type="submit"><i class="fa fa-search"></i></button>
+
+
+      </section>
+    </ul>
+  </nav>
+ 
+
+
+  <section class="footer">
+    <p>Tel. nummer: +31 6 12345678 </p>   
+    <p>Adres: Julianastraat 69, Plaats</p>
+    </section>
+    </footer>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bedrijfs website</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="shortcut icon" href="img/logo2.png">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  
+</head>
+
+<body>
+  <nav>
+    <ul>
+      <center>
+        <img src="img/zalando1.png" alt="logo" width="150" height="35">
+</center>
+      <li><a href="index.php">Home</a></li>
+      <li class="dropdown">
+        <a href="javascript:void(0)" class="schoenen1">Schoenen</a>
+        <section class="content">
+          <a href="dames.php">Dames</a>
+          <a href="heren.php">Heren</a>
+          <a href="kinderen.php">Kinderen</a>
+      </li>
+      <li><a href="contact.php">Contact</a></li>
+
+      <section class="searchbar">
+  <input type="text" placeholder="search..">
+  <button type="submit"><i class="fa fa-search"></i></button>
+
+
+      </section>
+    </ul>
+  </nav>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+
+<center>
+  <h1 class="h1">Welkom!</h1> <br>
+   <h2 class="h2">   Hier vindt u alle schoenen van dames, heren & kinderen<br>
+      Ook vindt u hier accessoires voor schoenen, zoals veters en schoenenspray
+      </center>
+      </h2>
+      <br>
+      <center>
+      <img src="img/schoenen.jpg" alt="schoenenhome" class="imghome" height="300" width="900">
+      </center>
+
+
+  <section class="footer">
+    <p>Tel. nummer: +31 6 12345678 </p>   
+    <p>Adres: Julianastraat 69, Plaats</p>
+    </section>
+    </footer>
+</body>
+
+</html>
